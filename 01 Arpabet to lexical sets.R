e5cefe6ef30bebb47fd643410cadc8433d180f34
Lars Hinrichs <wordsmith189@users.noreply.github.com>
2019-12-30 22:56:52 -0500

    Create 01 Arpabet to lexical sets.R

---
+++ 01 Arpabet to lexical sets.R
@@ -0,0 +1,30 @@
+
+# This script translates vowel labels from Arpabet (used by 
+# default in DARLA) to Wells lexical set labels.
+
+# Install (if necessary) and load pacman.
+if (!require(pacman)){
+  install.packages('pacman')
+  }
+library(pacman)
+
+# Install needed packages
+p_load(tidyverse, plyr)
+
+
+# !!! Data must be in memory as df. !!!
+
+# Change the labels in the vowels column to Wells lexical set labels.
+df$vowel <- mapvalues(df$vowel, 
+                      from = c("AA", "AE", "AH", "AO", 
+                               "AW", "AY", "EH", "ER", 
+                               "EY", "IH", "IY", "OW", 
+                               "OY", "UH", "UW"),
+                      to = c("LOT/PALM/START", "TRAP/BATH", "STRUT", "THOUGHT", 
+                             "MOUTH", "PRICE", "DRESS", "NURSE", 
+                             "FACE", "KIT", "FLEECE", "GOAT", 
+                             "CHOICE", "FOOT", "GOOSE"))
+
+
+
+
